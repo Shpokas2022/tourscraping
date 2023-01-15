d534f0c7824f6e9077d2e8d37a96b0fe65e58dda
Darius Vasionis <darius@vasionis.eu>
2023-01-15 15:36:18 +0200

    Exporting to exel all data

--- scrape_data.py
+++ scrape_data.py
@@ -1,53 +1,43 @@
 from bs4 import BeautifulSoup
 import requests
-from csv import writer
+# from csv import writer
+# from tqdm import tqdm
+import pandas as pd
 
-# Priskyriame mus dominantį puslapį
-url = 'https://www.kelioniulaikas.lt/keliones/autobusu/pazintines'
-# Nuskaitome visą svetainę
-page = requests.get(url)
+keliones = []
+# Iteruojame per visus puslapius
+for i in range(1, 6):
+    # Priskyriame mus dominantį puslapį
+    url = f'https://www.kelioniulaikas.lt/keliones/autobusu/pazintines?page={i}'
+    # Nuskaitome visą svetainę
+    page = requests.get(url)
 
-# Visas HTML dokumentas patalpintas bus į kintamąjį soup
-soup = BeautifulSoup(page.content, 'html.parser')
-# Paimame visas keliones į vieną konteinerį
-products = soup.find_all("article", class_="travel-grid")
-for product in products:
-    titles = product.find("div", class_ = "content-title")
-    title = titles.find("a").text
-    print(title)
+    # Visas HTML dokumentas patalpintas bus į kintamąjį soup
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # Paimame visas keliones į vieną konteinerį
+    products = soup.find_all("article", class_="travel-grid")
 
-# lists = soup.find_all('div', class_="row")
-# for list in lists:
-#     title = list.find('div', class_="").text
-#     print(title)
-    # countries = list.find('a', class_="address").text
-    # for country in countries:
-    #     state = country.find('a', class_='address')
-    
-    # cells = soup.find_all('div', class_='content')
-    # for cell in cells:
-    # trukme = list.find('div', class_="marks").text
-    # price = list.find('span', class_="date-price").text
-    # Removing Euro mark and converting text to float
-    # price = float(price[:-1])
-    # title = list.find('div', class_="content-title").text
-    # country = list.find('a', class_="address").text
-    # date = list.find('div', class_="col-4 col-date").text
-    # dates = list.find('div', class_="col-4 col-date").text
-    # date = dates.find("div", class_="col-4 col-date")
+    for product in products:
+        titles = product.find("div", class_ = "content-title")
+        title = titles.find("a").text
+        countries = product.find("ul", class_="countries-list")
+        country = countries.find("a", class_="address").text
+        days = product.find("div", class_="marks")
+        day = days.find_all("span")[1].text
+        day = int(day[:-2])
+        info1 = [title, country, day]
+        # Iteruojame per datas ir kainas
+        details = product.find_all("div", class_="row")
+        for detail in details:
+            dates = detail.find("div", class_="col-4 col-date").text
+            prices = detail.find(class_="date-price").text
+            # Nuimame valiutos (Euro) simbolį ir paverčiame tekstą į realų skaičių
+            price = float(prices[:-1])
+            ppd = price / day
+            price_per_day = "{:.2f}".format(ppd)
+            info2 = [dates, price, price_per_day]
+            keliones.append(info1+info2)
+            df = pd.DataFrame(keliones, columns=['Title','Country', 'Dates', 'Price, Euro', 'Days', 'Price per day, Eur'])
+            df.to_excel('kelioniu_laikas.xlsx', index=False)
 
-       
-    # print(countries)
-    # print (title)
-# with open('tours.csv', 'w', encoding='utf8', newline='') as f:
-#     thewriter = writer(f)
-#     header = ["City", "info", "Price", "Data", "Description"]
-#     thewriter.writerow(header)
-#     for list in lists:
-#         city = list.find('a', class_="link").text.replace('\n', '')
-#         info = list.find('div', class_ = "info-block hidden-xs").text.replace('\n', '')
-#         price = list.find('div', class_ = "price text-center pull-left").text.replace('\n', '')
-#         description = list.find('div', class_="offer-description clearfix").text.replace('\n', '')
-#         data = [city, info, price, description]
-#         thewriter.writerow(data)
 
